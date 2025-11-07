b535ca82ea631a86ef4ba4d474a17c632c737efd
abbybutts <abigail.butts@mohawkcollege.ca>
2025-11-07 12:01:41 -0500

    Setup player class.

---
+++ Player.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MohawkGame2D
+{
+    public class Player
+    {
+        Vector2 pos;
+        Vector2 size;
+        float speed;
+        float jumpHeight;
+
+        public Player(Vector2 pos, Vector2 size, float speed, float jumpHeight)
+        {
+            this.pos = pos;
+            this.size = size;
+            this.speed = speed;
+            this.jumpHeight = jumpHeight;
+        }
+
+
+        public void Setup()
+        {
+
+        }
+        public void Update()
+        {
+
+        }
+
+
+    }
+}
